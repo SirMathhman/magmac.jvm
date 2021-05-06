4b2bc273a0a9d3fb26ae0ea2038dcc1439ce1cd0
SirMathhman <sirmathhman@gmail.com>
2021-05-06 19:38:28 -0400

    Remove duplicates

--- src/test/java/magmac/ApplicationTest.java
+++ src/test/java/magmac/ApplicationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,21 +13,34 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ApplicationTest {
     @Test
     void should_write_content() throws IOException {
-        var path = run();
-        var actual = Files.readString(path);
+        var source = writeSource();
+        var target = run();
+        var actual = Files.readString(target);
         var expected = "#include <stdio.h>\nint main(){return 0;}";
         assertEquals(expected, actual);
-        Files.delete(path);
+        tearDown(source, target);
+    }
+
+    private Path writeSource() throws IOException {
+        var source = Paths.get(".", "main.mgs");
+        Files.writeString(source, "import native stdio\ndef main() : I16 => {return 0;}");
+        return source;
     }
 
     private Path run() throws IOException {
         return new Application().run();
     }
 
+    private void tearDown(Path source, Path target) throws IOException {
+        Files.delete(target);
+        Files.delete(source);
+    }
+
     @Test
     void should_write_file_if_not_exist() throws IOException {
-        var path = run();
-        assertTrue(Files.exists(path));
-        Files.delete(path);
+        var source = writeSource();
+        var target = run();
+        assertTrue(Files.exists(target));
+        tearDown(source, target);
     }
 }
