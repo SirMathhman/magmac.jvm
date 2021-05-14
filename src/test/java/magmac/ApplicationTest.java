3cdc3ae70cd512c6a20b3a87f884d88424050b66
SirMathhman <sirmathhman@gmail.com>
2021-05-13 20:23:20 -0400

    Add target file compilation

--- src/test/java/magmac/ApplicationTest.java
+++ src/test/java/magmac/ApplicationTest.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -19,6 +20,61 @@ public class ApplicationTest {
                 arguments("c", "Main.c"),
                 arguments("c", "Main.h")
         );
+    }
+
+    @ParameterizedTest
+    @MethodSource("createTargets")
+    void should_exist(String parent, String child) throws IOException {
+        var dist = Paths.get(".", "dist");
+        ensureDirectory(dist);
+
+        var js = dist.resolve("js");
+        ensureDirectory(js);
+        var jsTarget = js.resolve("Main.js");
+        ensureFile(jsTarget);
+        var jsHeader = js.resolve("Main.d.ts");
+        ensureFile(jsHeader);
+
+        var c = dist.resolve("c");
+        ensureDirectory(c);
+        var cTarget = c.resolve("Main.c");
+        ensureFile(cTarget);
+        var cHeader = c.resolve("Main.h");
+        ensureFile(cHeader);
+
+        assertFalse(Files.exists(dist.resolve(parent).resolve(parent).resolve(child)));
+
+        Files.walkFileTree(dist, new FileVisitor<>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.deleteIfExists(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.deleteIfExists(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    private void ensureDirectory(Path dist) throws IOException {
+        if (!Files.exists(dist)) Files.createDirectory(dist);
+    }
+
+    private void ensureFile(Path child) throws IOException {
+        if (!Files.exists(child)) Files.createFile(child);
     }
 
     @ParameterizedTest
